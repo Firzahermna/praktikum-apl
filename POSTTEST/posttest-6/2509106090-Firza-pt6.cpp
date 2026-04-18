86a5a3d4d064245b9143867e2cd946b2bbe95abc
Firzahermna <firzahermana467@gmail.com>
2026-04-18 22:01:14 +0800

    pt6

---
+++ POSTTEST/posttest-6/2509106090-Firza-pt6.cpp
@@ -0,0 +1,284 @@
+#include <iostream>
+using namespace std;
+
+struct User {
+    string username;
+    string password;
+};
+
+struct Gaming {
+    string nama_peralatan;
+    int harga;
+    int stok;
+};
+
+#define MAX 100
+Gaming daftarGaming[MAX];
+int totalData = 0;
+
+void tambahData(Gaming *data, int &total) {
+    if (total < MAX) {
+        cout << "Masukkan Nama Peralatan Gaming : ";
+        cin.ignore();
+        getline(cin, (data + total)->nama_peralatan);
+        cout << "Masukkan Harga : ";
+        cin >> (data + total)->harga;
+        cout << "Masukkan Stok : ";
+        cin >> (data + total)->stok;
+        total++;
+        cout << "Berhasil menambahkan data!\n";
+    } else {
+        cout << "Kapasitas penuh!\n";
+    }
+}
+
+void lihatData(Gaming *data, int total) {
+    if (total == 0) {
+        cout << "Data masih kosong\n";
+    } else {
+        cout << "\n----- Daftar Peralatan Gaming -----\n";
+        for (int i = 0; i < total; i++) {
+            cout << i + 1 << ". " << (data + i)->nama_peralatan
+                << " | Harga: Rp" << (data + i)->harga
+                << " | Stok: " << (data + i)->stok << endl;
+        }
+    }
+}
+
+void ubahData(Gaming *data, int total) {
+    int index;
+    if (total == 0) {
+        cout << "Tidak ada data yang bisa diubah.\n";
+    } else {
+        lihatData(data, total);
+        cout << "Masukkan nomor data yang ingin diubah: ";
+        cin >> index;
+        if (index > 0 && index <= total) {
+            cout << "Masukkan Nama Baru : ";
+            cin.ignore();
+            getline(cin, (data + index - 1)->nama_peralatan);
+            cout << "Masukkan Harga Baru : ";
+            cin >> (data + index - 1)->harga;
+            cout << "Masukkan Stok Baru : ";
+            cin >> (data + index - 1)->stok;
+            cout << "Data berhasil diperbarui!\n";
+        } else {
+            cout << "Nomor tidak ada\n";
+        }
+    }
+}
+
+int hapusData(Gaming *data, int total) {
+    int index;
+    if (total == 0) {
+        cout << "Tidak ada data untuk dihapus.\n";
+        return total;
+    }
+
+    lihatData(data, total);
+    cout << "Masukkan nomor data yang ingin dihapus: ";
+    cin >> index;
+
+    if (index > 0 && index <= total) {
+        for (int i = index - 1; i < total - 1; i++) {
+            *(data + i) = *(data + i + 1);
+        }
+        total--;
+        cout << "Data berhasil dihapus!\n";
+    } else {
+        cout << "Nomor tidak ada\n";
+    }
+
+    return total;
+}
+
+void urutNama(Gaming *data, int total) {
+    for (int i = 0; i < total - 1; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+            if ((data + j)->nama_peralatan < (data + j + 1)->nama_peralatan) {
+                Gaming temp = *(data + j);
+                *(data + j) = *(data + j + 1);
+                *(data + j + 1) = temp;
+            }
+        }
+    }
+    cout << "Sorting nama berhasil!\n";
+}
+
+void urutHarga(Gaming *data, int total) {
+    for (int i = 0; i < total - 1; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+            if ((data + j)->harga > (data + j + 1)->harga) {
+                Gaming temp = *(data + j);
+                *(data + j) = *(data + j + 1);
+                *(data + j + 1) = temp;
+            }
+        }
+    }
+    cout << "Sorting harga berhasil!\n";
+}
+
+void urutStok(Gaming *data, int total) {
+    for (int i = 0; i < total - 1; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+            if ((data + j)->stok < (data + j + 1)->stok) {
+                Gaming temp = *(data + j);
+                *(data + j) = *(data + j + 1);
+                *(data + j + 1) = temp;
+            }
+        }
+    }
+    cout << "Sorting stok berhasil!\n";
+}
+
+void sortHargaAsc(Gaming *data, int total) {
+    for (int i = 0; i < total - 1; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+            if ((data + j)->harga > (data + j + 1)->harga) {
+                Gaming temp = *(data + j);
+                *(data + j) = *(data + j + 1);
+                *(data + j + 1) = temp;
+            }
+        }
+    }
+}
+
+void cariNama(Gaming *data, int total) {
+    string cari;
+    bool found = false;
+
+    cout << "Masukkan nama peralatan: ";
+    cin.ignore();
+    getline(cin, cari);
+
+    for (int i = 0; i < total; i++) {
+        if ((data + i)->nama_peralatan == cari) {
+            cout << "Data ditemukan:\n";
+            cout << (data + i)->nama_peralatan
+                << " | Harga: Rp" << (data + i)->harga
+                << " | Stok: " << (data + i)->stok << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) cout << "Data tidak ditemukan\n";
+}
+
+void cariHarga(Gaming *data, int total) {
+    int target;
+    cout << "Masukkan harga yang dicari: ";
+    cin >> target;
+
+    sortHargaAsc(data, total);
+
+    int low = 0, high = total - 1;
+    bool found = false;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if ((data + mid)->harga == target) {
+            cout << "Data ditemukan:\n";
+            cout << (data + mid)->nama_peralatan
+                << " | Harga: Rp" << (data + mid)->harga
+                << " | Stok: " << (data + mid)->stok << endl;
+            found = true;
+            break;
+        } else if ((data + mid)->harga < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found) cout << "Data tidak ditemukan\n";
+}
+
+int main() {
+
+    User admin = {"Firza","090"};
+    string input_nama, input_pw;
+    int percobaan = 0;
+    bool login_berhasil = false;
+
+    while (percobaan < 3) {
+        cout << "-------LOGINKAN DOLO-------" << endl;
+        cout << "Masukkan Nama: ";
+        cin >> input_nama;
+        cout << "Masukkan Password: ";
+        cin >> input_pw;
+
+        if (input_nama == admin.username && input_pw == admin.password) {
+            login_berhasil = true;
+            cout << "Selamat datang! bos besarku, " << input_nama << endl;
+            break;
+        } else {
+            percobaan++;
+            cout << "Nama atau Password salah, sisa percobaan: " << 3 - percobaan << endl << endl;
+        }
+    }
+
+    if (!login_berhasil) {
+        cout << "Telah gagal login 3 kali.\n";
+        return 0;
+    }
+
+    int pilihan;
+    do {
+        cout << "\n===== MENU UTAMA TOKO PERALATAN GAMING =====\n";
+        cout << "1. Tambah Data\n";
+        cout << "2. Lihat Data\n";
+        cout << "3. Ubah Data\n";
+        cout << "4. Hapus Data\n";
+        cout << "5. Urutkan nama Z-A\n";
+        cout << "6. Urutkan Harga Murah-Mahal\n";
+        cout << "7. Urutkan Stok dari yang Terbanyak\n";
+        cout << "8. Cari Nama \n"; //Linear
+        cout << "9. Cari Harga \n"; //Binary
+        cout << "10. Keluar\n";
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+        case 1:
+            tambahData(daftarGaming, totalData);
+            break;
+        case 2:
+            lihatData(daftarGaming, totalData);
+            break;
+        case 3:
+            ubahData(daftarGaming, totalData);
+            break;
+        case 4:
+            totalData = hapusData(daftarGaming, totalData);
+            break;
+        case 5:
+            urutNama(daftarGaming, totalData);
+            lihatData(daftarGaming, totalData);
+            break;
+        case 6:
+            urutHarga(daftarGaming, totalData);
+            lihatData(daftarGaming, totalData);
+            break;
+        case 7:
+            urutStok(daftarGaming, totalData);
+            lihatData(daftarGaming, totalData);
+            break;
+        case 8:
+            cariNama(daftarGaming, totalData);
+            break;
+        case 9:
+            cariHarga(daftarGaming, totalData);
+            break;
+        case 10:
+            cout << "Terima kasih telah berbelanja!\n";
+            break;
+        default:
+            cout << "Pilihan tidak ada\n";
+        }
+
+    } while (pilihan != 10);
+
+    return 0;
+}
